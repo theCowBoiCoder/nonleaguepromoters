b7ff685ad74d923888db8769393a8176bf46ee42
Hayden Sweet <haydensweet29@gmail.com>
2021-02-15 21:10:51 +0000

    mobile bg fix

--- resources/views/pages/register.blade.php
+++ resources/views/pages/register.blade.php
@@ -1,6 +1,6 @@
 @extends('layout.main')
 @section('content')
-    <div class="bg-home-image bg-black bg-scroll bg-cover h-screen">
+    <div class="bg-home-image bg-black bg-scroll bg-cover lg:h-screen">
         <div class="container m-auto">
             <div class="text-center pt-10 pb-10 lg:pt-20 lg:pb-20 lg:pl-20 lg:text-left">
                 <h1 class="uppercase text-white text-5xl lg:text-8xl bolder italic font-Roboto leading-8"
