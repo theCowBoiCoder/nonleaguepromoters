ecff6371958443a04b0943028f1a9fdaa0967805
Hayden Sweet <haydensweet29@gmail.com>
2021-03-02 09:47:49 +0000

    Centered the div

--- resources/views/pages/landing.blade.php
+++ resources/views/pages/landing.blade.php
@@ -1,6 +1,6 @@
 @extends('layout.main')
 @section('content')
-<div class="bg-home-image bg-black bg-scroll bg-cover bg-center h-screen">
+<div class="bg-home-image bg-black bg-scroll bg-cover bg-center h-screen flex">
     <div class="container m-auto">
         <div class="text-center pt-10 pb-10 lg:pt-20 lg:pb-20 lg:pl-20 lg:text-left">
             <h1 class="uppercase text-white text-5xl lg:text-8xl bolder italic font-Roboto" style="color: orange; font-weight: 900;">The Non <br>League <br>Promoters</h1>
