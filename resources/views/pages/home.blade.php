8342604a42e9f9370cf31b92fbf718162e1a0fe7
Hayden Sweet <haydensweet29@gmail.com>
2021-03-31 19:36:36 +0100

    Home page added

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,6 +1,6 @@
 @extends('layout.main')
 @section('content')
-    <div class="md:bg-home-image bg-black bg-scroll bg-cover bg-center h-screen">
+    <div class="md:bg-home-image bg-black bg-scroll bg-cover bg-center bg-mobile-image md:h-screen">
         @include('layout.login')
 
         <div class="container m-auto">
