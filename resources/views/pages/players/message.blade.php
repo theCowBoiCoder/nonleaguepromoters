457b4dcc8a97ecabd194f12a83b2bd673c9f0dbd
Hayden Sweet <haydensweet29@gmail.com>
2021-04-02 20:55:03 +0100

    Missed a couple of pages.

--- resources/views/pages/players/message.blade.php
+++ resources/views/pages/players/message.blade.php
@@ -7,6 +7,7 @@
         @include('layout.login')
         <div class="container m-auto">
             <div class="text-center pt-10 pb-10 lg:pt-20 lg:pb-20 lg:pl-20 lg:text-left">
+                @include('pages.partials.mobile_image')
                 <h1 class="uppercase text-white text-5xl lg:text-8xl bolder italic font-Roboto xs:leading-8"
                     style="color: orange; font-weight: 900;">Send {{$player->name}} <br> A Message</h1>
                 <h3 class="uppercase text-white text-1xl lg:text-2xl bolder italic font-Roboto leading-8"
--- resources/views/pages/players/players.blade.php
+++ resources/views/pages/players/players.blade.php
@@ -4,9 +4,7 @@
         @include('layout.login')
         <div class="container m-auto">
             <div class="text-center pt-10 pb-10 lg:pt-20 lg:pb-20 lg:pl-20 lg:text-left">
-                <div class="flex justify-center visible md:invisible mb-3">
-                    <div><img src="{{asset('images/mobile_logo.jpg')}}" alt="" width="150"></div>
-                </div>
+                @include('pages.partials.mobile_image')
                 <h1 class="uppercase text-white text-5xl lg:text-8xl bolder italic font-Roboto xs:leading-8"
                     style="color: orange; font-weight: 900;">Player <br>Search <br>Platform</h1>
                 <h3 class="uppercase text-white text-1xl lg:text-2xl bolder italic font-Roboto leading-8"
