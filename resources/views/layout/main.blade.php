be74311861f818ed01edf2e36e01e60e85b98ee8
Hayden Sweet <haydensweet29@gmail.com>
2021-06-27 09:28:07 +0100

    Google Code added.

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -7,6 +7,15 @@
     <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css">
     <title>{{env('APP_NAME')}}</title>
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-1ESY29H823"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-1ESY29H823');
+    </script>
 </head>
 <body>
 <div class="mx-auto">
