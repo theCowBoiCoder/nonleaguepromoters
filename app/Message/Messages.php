8601a3e090ae99dd495f5147c0f2046b56842d31
Hayden Sweet <haydensweet29@gmail.com>
2021-06-30 13:10:13 +0100

    Notification send!

--- app/Message/Messages.php
+++ app/Message/Messages.php
@@ -3,12 +3,15 @@
 namespace App\Message;
 
 use App\Models\Message;
+use App\Models\User;
+use App\Notifications\MessageSendNotification;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Notification;
 
 class Messages
 {
-    public static function send($user_id, Request $request)
+    public static function send($user_id, Request $request): string
     {
         Message::query()->create([
             'sender_user_id' => (Auth::user() != null) ? Auth::user()->id : NULL,
@@ -17,6 +20,9 @@ class Messages
             'message' => encrypt($request->message),
             'from' => $request->from_name
         ]);
+
+        $user = User::query()->find($user_id);
+        Notification::route('mail', $user->email)->notify(new MessageSendNotification());
 
         return 'Your message has been sent';
     }
