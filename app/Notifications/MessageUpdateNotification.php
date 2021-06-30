2cb77a4f26ec536c84d839234501764843a56f88
Hayden Sweet <haydensweet29@gmail.com>
2021-06-30 20:13:35 +0100

    Added in name

--- app/Notifications/MessageUpdateNotification.php
+++ app/Notifications/MessageUpdateNotification.php
@@ -42,7 +42,7 @@ class MessageUpdateNotification extends Notification
     {
         return (new MailMessage)
             ->subject('You have outstanding messages')
-            ->greeting('Hello there')
+            ->greeting("Hello {$this->user->name}")
             ->line('You have some outstanding messages to read')
             ->action('View My Messages', url("https://thenonleaguepromoters.co.uk/profile/{$this->user->id}/my-messages"));
     }
